e4f3dc6b6d0ffbc939eee0735e4a48cc532f2c68
Jim O'Regan <joregan@gmail.com>
2017-06-14 09:42:01 +0000

    non-Irish names should not be lenited or eclipsed by default; accept them, because nobody speaks standard Irish

--- dev/addpn.pl
+++ dev/addpn.pl
@@ -19,14 +19,12 @@ while(<>) {
     my $lm = $first . $rest;
     print "<e lm=\"$lm\"><i>$lm</i><par n=\"${par}__np\"/></e>\n";
         if ($first =~ /[BCDFGMPSTK]/ && $second ne 'h') {
-            print "<e lm=\"$lm\"><par n=\"${first}__len\"/><i>$rest</i><par n=\"${par}__len\"/></e>\n";
-        } else {
-            print "<e lm=\"$lm\"><i>${lm}</i><par n=\"${par}__len\"/></e>\n";
+            print "<e lm=\"$lm\" r=\"LR\"><par n=\"${first}__len\"/><i>$rest</i><par n=\"${par}__len\"/></e>\n";
         }
-        if ($first =~ /[BCDFGPTAEIOUÁÉÍÓÚK]/) {
-            print "<e lm=\"$lm\"><par n=\"${first}__ecl\"/><i>$rest</i><par n=\"${par}__ecl\"/></e>\n";
-        } else {
-            print "<e lm=\"$lm\"><i>${first}${rest}</i><par n=\"${par}__ecl\"/></e>\n";
+        print "<e lm=\"$lm\"><i>${lm}</i><par n=\"${par}__len\"/></e>\n";
+        if ($first =~ /[BDFGPTAEIOUÁÉÍÓÚK]/ || ($first eq 'C' && $second ne 'h')) {
+            print "<e lm=\"$lm\" r=\"LR\"><par n=\"${first}__ecl\"/><i>$rest</i><par n=\"${par}__ecl\"/></e>\n";
         }
+        print "<e lm=\"$lm\"><i>${first}${rest}</i><par n=\"${par}__ecl\"/></e>\n";
 
 }
